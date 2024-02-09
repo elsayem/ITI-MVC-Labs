7ea43933b595d2462e32423aaf5da065e32cd160
asaiem <eng.abdulrahman.alsaiem@gmail.com>
2024-02-09 15:47:20 +0200

    Add Lab-01

---
+++ Lab-01/Lab-01/Controllers/NameController.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Drawing;
+
+namespace Lab_01.Controllers
+{
+    public class DataController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public string welcome(string n)
+        {
+            return $"Welcome {n}";
+        }
+        public string sum(int x, int y)
+        {
+            //return $"{x} + {y} = {x+y}" ;
+            int res = x + y;
+            return $"{x} + {y} = {res}";
+
+            //return res;
+        }
+
+
+
+        #region Using Int return value type
+        //Using INT as return value
+        //public int sum(int x, int y)
+        //{
+
+        //    int res = x + y;
+        //    //return $"{x} + {y} = {res}";
+
+        //    return res;
+        //}
+        #endregion
+
+
+
+
+        //public JsonResult JsSum(int x, int y) {
+        //    //return $"{x} + {y} = {x+y}" ;
+        //    var res = x + y;
+        //    //return $"{x} + {y} = {x + y}";
+
+        //    return Json(res, JsonRequestBehavior.AllowGet);
+        //}
+    }
+}
---
+++ Lab-01/Lab-01/Controllers/StudentController.cs
@@ -0,0 +1,19 @@
+﻿using Lab_01.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Lab_01.Controllers
+{
+    public class StudentController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public ViewResult StdView()
+        {
+            Student sudent01 = new Student() { id= 1, name="Abdulrahman", age=25};
+            return View(sudent01);
+        }
+    }
+}
